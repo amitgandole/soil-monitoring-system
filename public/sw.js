474f9d4393d1b60cc0fb5095f7a72e77c2b31037
amitgandole <35072407+amitgandole@users.noreply.github.com>
2019-04-11 17:49:53 +0530

    Deletion of Existing

--- public/sw.js
+++
@@ -1,6 +0,0 @@
-if ("serviceWorker" in navigator) {
-    navigator.serviceWorker
-      .register("/../service-worker.js")
-      .then(console.log)
-      .catch(console.error);
-  }
--- routes/userprofile.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    userprofileaccess: (req, res) => {
-        var data = require('url').parse(req.url, true).query;
-        res.render('userprofile',{uname1:data.username});
-        res.redirect('userprofile.ejs');
-         
-    }
-};
